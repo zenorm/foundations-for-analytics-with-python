f319f4d96d72c347597e57ab7aad08118587f600
zenorm <werty743@gmail.com>
2022-11-09 22:27:47 +0100

    Update my_first_scrpit.py

--- my_first_scrpit.py
+++ my_first_scrpit.py
@@ -1,5 +1,13 @@
-print ("Hello, world!")
-a = [1, 2, 3, 4]
-b = ["first", "second", "third", "fourth"]
-c = a + b
-print("Output #3: {0}, {1}, {2}".format(a, b, c))
+string1 = "My deliverable is due in May"
+string1_list1 = string1.split()
+string1_list2 = string1.split(" ",2)
+print("Output #21: {0}".format(string1_list1))
+print("Output #22: FIRST PIECE:{0} SECOND PIECE:{1} THIRD PIECE:{2}"\
+.format(string1_list2[0], string1_list2[1], string1_list2[2]))
+string2 = "Your,deliverable,is,due,in,June"
+string2_list = string2.split(',')
+print("Output #23: {0}".format(string2_list))
+print("Output #24: {0} {1} {2}".format(string2_list[1], string2_list[5],\
+string2_list[-1]))
+
+print("Output #25: {0}".format(','.join(string2_list)))
