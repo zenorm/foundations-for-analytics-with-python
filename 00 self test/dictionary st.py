4d10be4b3d383bdaad12148847dbc06483aa5536
zenorm <werty743@gmail.com>
2022-11-10 20:18:29 +0100

    Create dictionary st.py

---
+++ 00 self test/dictionary st.py
@@ -0,0 +1,8 @@
+empty_dict = {}
+a_dict = {"one":1, "two":2, "three":3}
+print("output #102:{}".format(a_dict))
+print("output #103:a_dict has {!s}elements".format(len(a_dict)))
+
+another_dict = {"x":"printer", "y":5, "z":['star', 'circle', 9]}
+print("output #104:{}".format(another_dict))
+print("output #105: another_dict also has {!s} elements".format(len(another_dict)))
