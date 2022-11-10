75ce7689c75e807a9c23e0f307c6a9161368140b
zenorm <werty743@gmail.com>
2022-11-10 23:34:35 +0100

    added more txt

---
+++ 00_self_test/os_glob.py
@@ -0,0 +1,9 @@
+import os
+import sys
+import glob
+
+print("output #145:")
+inputPath = sys.argv[1]
+for input_file in glob.glob(os.path.join(inputPath,'*.txt')):
+    with open(input_file,'r', newline = '') as filereader:
+        print("{}".format(row.strip()))
